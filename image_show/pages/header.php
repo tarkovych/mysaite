27067cdf082759588f4744bd8da9ecb7a351ed44
tarkovych <tarkovychne2@gmail.com>
2019-04-24 09:47:55 +0300

    09:47

--- image_show/pages/header.php
+++ image_show/pages/header.php
@@ -23,6 +23,7 @@
 
 
 
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="index.php">
     ГОЛОВНА
@@ -30,4 +31,66 @@
   <a class="navbar-brand" href="program.php">
     ADMIN
   </a> 
+  <!-- <div id="languages-switcher">
+
+
+				<div id="languages-switcher-btn">
+					<script>document.write('<div id="current-language" class="flag flag-' + currentLanguage + '"></div>');</script>
+				</div>
+				<div id="languages-dropdown-list">
+					<ul>
+          <li><div data-code="ru" class="flag flag-ru">Русский</div></li>
+          <li><div data-code="ua" class="flag flag-ua">Українська</div></li>
+          <li><div data-code="en" class="flag flag-en">English</div></li>
+					</ul>
+				</div>
+			</div> -->
+
+
+
+
+
+
 </nav>
+
+<!-- <script>
+var languages = ['<div data-code="ru" class="flag flag-ru">Русский</div>',
+'<div data-code="ua" class="flag flag-ua">Українська</div>',
+'<div data-code="en" class="flag flag-en">English</div>'] ; 
+function getCookie(name){
+	var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+function setCookie(name, value, options){
+	options = options || {};
+	var expires = options.expires;
+	if(typeof expires == "number" && expires){
+		var d = new Date();
+		d.setTime(d.getTime() + expires * 1000);
+		expires = options.expires = d;
+	}
+	if(expires && expires.toUTCString) options.expires = expires.toUTCString();
+	value = encodeURIComponent(value);
+	var updatedCookie = name + "=" + value;
+	for(var propName in options){
+		updatedCookie += "; " + propName;
+		var propValue = options[propName];
+		if(propValue !== true) updatedCookie += "=" + propValue;
+	}
+	document.cookie = updatedCookie;
+}
+function deleteCookie(name){
+	setCookie(name, "", {expires: -1});
+}
+if(config.showLanguagesSwitcher === false) document.write('<style>#top-bar-languages-switcher{display:none}#top-bar-version{right:0}#top-bar-left{padding-right:60px}</style>');
+if(config.showQuestionNames === false) document.write('<style>.qName{display:none}</style>');
+if(config.showDebugButton === true) document.write('<style>#debug-toggle{display:inline-block}</style>');
+</script>
+<style>{display:none}</style>
+
+<script>
+var currentLanguage = getCookie('language') || config.defaultLanguage;
+document.write('<style>.' + currentLanguage + '{display:inline}</style>');
+
+
+</script> -->
