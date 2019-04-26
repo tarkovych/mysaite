1844c1bd3af8dd1736dcd8424b64c61890f0c453
tarkovych <tarkovychne2@gmail.com>
2019-04-26 18:32:11 +0300

    18:32

--- image_show/php/connection.php
+++ image_show/php/connection.php
@@ -1,12 +1,19 @@
 <?php
-// $dbhost = "mysql.zzz.com.ua"; // Имя хоста БД
-// $dbusername = "tarkovychDB"; // Пользователь БД
-// $dbpass = "dbVanyabiluy1122"; // Пароль к базе
-// $dbname = "tarkovych"; // Имя базы
 
-$dbhost = "localhost"; // Имя хоста БД
-$dbusername = "root"; // Пользователь БД
-$dbpass = ""; // Пароль к базе
-$dbname = "ImageShow"; // Имя базы
+// $dbhost = "localhost"; // Имя хоста БД
+// $dbusername = "root"; // Пользователь БД
+// $dbpass = ""; // Пароль к базе
+// $dbname = "ImageShow"; // Имя базы
 
+ $dbhost = "http://web.brt.org.ua"; // Имя хоста БД
+ $dbusername ="brt-db"; // Пользователь БД
+ $dbpass = "tASupAKek9"; // Пароль к базе
+ $dbname = "brt-db_ImageShow"; // Имя базы
+
+
+
+ 	
+//tASupA8Eke	brt-web
+
+//http://web.brt.org.ua/myadmin/index.php
 ?>
