b39ffe035c3506be367eb90be3d7afdf092e8648
tarkovych <tarkovychne2@gmail.com>
2019-05-28 15:12:55 +0300

    Auto stash before merge of "master" and "origin/master"

--- mailer/comp/navbar.php
+++ mailer/comp/navbar.php
@@ -14,14 +14,18 @@ $Navpages = [
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $Navpages["title"][$PagesValue]; ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/myStyle.css">
+
+    <title><?php echo $Navpages["title"][$PagesValue]; ?></title>
 </head>
 
 <body>
-
+<div class="MyNavbar">
     <header>
         <nav>
             <?php foreach ($Navpages["action"] as $key => $value) : ?>
@@ -32,6 +36,9 @@ $Navpages = [
             <?php endforeach;  ?>
         </nav>
     </header>
+</div>
+
+
     <style>
         nav a {
             text-decoration: none
